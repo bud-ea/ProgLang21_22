a7297964a3c8ad6a12564ba188bba74f3799fc7d
Евгения Будылина <bud-ea@yandex.ru>
2021-10-01 13:58:12 +0300

    +lab2 352

--- Surname_211_352/Surname_211_352/LabRab2/LabRab2.cpp
+++ Surname_211_352/Surname_211_352/LabRab2/LabRab2.cpp
@@ -1,11 +1,48 @@
 // LabRab2.cpp: определяет точку входа для консольного приложения.
 //
 
+#include <iostream>
 
+void task_triangle()
+{
+	std::cout << "task_tr";
+}
 
+void task_Cnk()
+{
+	std::cout << "task_Cnk";
+}
+void task_average()
+{
+	std::cout << "task_av";
+}
 
 int main()
 {
-    return 0;
+	int choice;
+    
+	while (true)
+	{
+		std::cout << "Menu: \n1. Triangle\n2. Cnk\n3. Average\n Input choice:";
+		std::cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			task_triangle();
+			break;
+		case 2:
+			task_Cnk();
+			break;
+		case 3:
+			task_average();//после последнего case break не несет смысловой нагрузки, но писать нужно
+			break;
+		default:
+			return 0;
+		}
+
+
+	}
+    
+	return 0;
 }
 
